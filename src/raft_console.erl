c77b92e19b5162a4a215390997c87f9826bf0306
Jan Skrzypczak <skrzypczak@zib.de>
2019-01-24 12:50:09 +0100

    added wrapper interface

---
+++ src/raft_console.erl
@@ -0,0 +1,28 @@
+%% Some wrapper for rabbitmq/ra
+%%
+
+-module(raft_console).
+
+-export([create/1, status/0]).
+
+-export([read/0, inc/0]).
+
+create(ErlangNodes) ->
+    ServerIds = [{counter_server, N} || N <- ErlangNodes],
+
+    {ok, ServersStarted, []} = ra:start_cluster(counter_cluster,
+                                                {simple, fun erlang:'+'/2, 0},
+                                                ServerIds),
+    ServersStarted.
+
+
+%% Assumes that this node is included in the ErlangNodes list given in create...
+status() -> ra:members({counter_server, node()}).
+
+read() ->
+    {ok, Result, _Leader} = ra:consistent_query({counter_server, node()}, fun (S) -> S end),
+    Result.
+
+inc() ->
+    {ok, _Reply, _Leader} = ra:process_command({counter_server, node()}, 1),
+    ok.
